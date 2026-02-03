b1b1428d4ad70aca27b4dc3b3d5cb0bae2876d6a
QosmuratSamat <qosmuratsamat@gmail.com>
2026-02-03 14:48:16 +0500

    Delete frontend directory

--- frontend/components/layout/index.ts
+++
@@ -1,5 +0,0 @@
-export { Navbar } from "./navbar";
-export { Footer } from "./footer";
-export { SidebarNav } from "./sidebar-nav";
-export { TopBar } from "./top-bar";
-export { AppShell } from "./app-shell";
--- frontend/schemas/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./auth";
-export * from "./contact";
-export * from "./course";
-export * from "./assignment";
-export * from "./profile";
